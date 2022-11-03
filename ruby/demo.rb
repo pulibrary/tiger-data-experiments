37c7354f5eb061d204b64dad767e08400692fe4a
mccalluc <mccalluc@users.noreply.github.com>
2022-11-03 16:28:25 -0400

    create -> delete

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -11,11 +11,34 @@ config = YAML.load_file('config.yaml').map { |k, v| [k.to_sym, v] }.to_h
 mf = MFClient.new(**config)
 
 mf.session() do
-  puts "Namespaces:"
+  puts "List namespaces:"
   doc = mf.call("asset.namespace.list")
   doc.elements["//namespace"].each do |el|
     puts el.text
   end
+
+  # Based on https://docs.google.com/presentation/d/1GAt7tr2Vb-KJtILbcEcMlr71qxVo0j5-/edit#slide=id.p23
+  puts "\nCreate asset:"
+  doc = mf.call("asset.create", "<name>dog</name>")
+  puts doc
+  id = doc.elements["//id"].first
+
+  puts "\nGet asset:"
+  doc = mf.call("asset.get", "<id>#{id}</id>")
+  puts doc
+
+  puts "\nSet metadata:"
+  doc = mf.call("asset.set", "<id>#{id}</id>",
+    "<meta><mf-note><note>Hello</note></mf-note></meta>")
+  puts doc
+  
+  puts "\nView original:"
+  doc = mf.call("asset.get", %Q{<id version="1">#{id}</id>})
+  puts doc
+
+  puts "\nDelete:"
+  doc = mf.call("asset.destroy", %Q{<id>#{id}</id>})
+  puts doc
 end
 
 # response_doc = mf_client.call("asset.namespace.list", session: session)
