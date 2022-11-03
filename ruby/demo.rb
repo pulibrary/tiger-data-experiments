41558311fdc9841c63be628eb6f89a9967559e03
mccalluc <mccalluc@users.noreply.github.com>
2022-11-03 12:30:54 -0400

    successful logon!

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -4,6 +4,27 @@ require 'yaml'
 require 'net/http'
 
 config = YAML.load_file('config.yaml')
-puts(config)
 
-# Net::HTTP.get('example.com', '/index.html')
+# Based on curl example from page 14 of "Mediaflux Developer Guide":
+
+request_xml = %Q{<request>
+  <service name="system.logon">
+    <args>
+      <domain>#{config["mf_domain"]}</domain>
+      <user>#{config["mf_username"]}</user>
+      <password>#{config["mf_password"]}</password>
+    </args>
+  </service>
+</request>}
+
+puts request_xml
+
+https = Net::HTTP.new(config["mf_host"], config["mf_port"])
+https.use_ssl = true
+request = Net::HTTP::Post.new("__mflux_svc__")
+request.body = request_xml
+request["Content-Type"] = "text/xml; charset=utf-8"
+
+response = https.request(request)
+
+puts response
