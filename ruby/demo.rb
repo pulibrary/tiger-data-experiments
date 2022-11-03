6518a832c5337564430248397c485069970aabcd
mccalluc <mccalluc@users.noreply.github.com>
2022-11-03 15:54:33 -0400

    pull names with expath

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -9,11 +9,14 @@ require './mf_client'
 # Hash with symbolic keys, but avoid extra Rails dependencies for now: 
 config = YAML.load_file('config.yaml').map { |k, v| [k.to_sym, v] }.to_h
 
-mf_client = MFClient.new(**config)
+mf = MFClient.new(**config)
 
-mf_client.session() do
-  response_doc = mf_client.call("asset.namespace.list")
-  puts response_doc
+mf.session() do
+  puts "Namespaces:"
+  response_doc = mf.call("asset.namespace.list")
+  response_doc.elements["//namespace"].each do |el|
+    puts el.text
+  end
 end
 
 # response_doc = mf_client.call("asset.namespace.list", session: session)
