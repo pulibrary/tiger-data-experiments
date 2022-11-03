417bce7d2159ad09318ff179d844f1a6866eb566
mccalluc <mccalluc@users.noreply.github.com>
2022-11-03 12:53:58 -0400

    Get response body

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -21,10 +21,12 @@ puts request_xml
 
 https = Net::HTTP.new(config["mf_host"], config["mf_port"])
 https.use_ssl = true
+https.read_timeout = 3 # I often forget the VPN, so set this low.
+https.continue_timeout = 3
 request = Net::HTTP::Post.new("__mflux_svc__")
 request.body = request_xml
 request["Content-Type"] = "text/xml; charset=utf-8"
 
 response = https.request(request)
 
-puts response
+puts response.body
