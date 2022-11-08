36a736bbab846e7a89eff9704cbcb58577f60426
mccalluc <mccalluc@users.noreply.github.com>
2022-11-08 11:38:32 -0500

    auto-correct

--- homebaked/mediaflux.rb
+++ homebaked/mediaflux.rb
@@ -2,12 +2,12 @@ require 'net/http'
 require 'rexml/document'
 
 module MediaFlux
-
   class MFError < StandardError
   end
 
   class MFApiError < MFError
     attr_reader :error, :message, :stack
+
     def initialize(response_doc)
       reply = response_doc.elements['/response/reply']
       @error = reply.elements['error'].text
@@ -21,62 +21,63 @@ module MediaFlux
 
   class MFClient
     def initialize(
-        verbose: false, allowed_services: [],
-        mf_host:, mf_port:, mf_domain:, mf_username:, mf_password:)
+      mf_host:, mf_port:, mf_domain:, mf_username:, mf_password:, verbose: false, allowed_services: []
+    )
       @verbose = verbose
       @mf_host = mf_host
       @mf_port = mf_port
       @mf_domain = mf_domain
       @mf_username = mf_username
       @mf_password = mf_password
-      @allowed_services = allowed_services + [
-        :logon_system, :logoff_system,
-        :list_asset_namespace,
-        :create_asset, :get_asset, :set_asset, :destroy_asset
+      @allowed_services = allowed_services + %i[
+        logon_system logoff_system
+        list_asset_namespace
+        create_asset get_asset set_asset destroy_asset
       ]
     end
 
     def method_missing(service_sym, **args)
-      if not @allowed_services.include? service_sym
-        raise MediaFlux::MFServiceError.new("'#{service_sym}' is not an allowed service")
+      unless @allowed_services.include? service_sym
+        raise MediaFlux::MFServiceError, "'#{service_sym}' is not an allowed service"
       end
-      service = service_sym.to_s.split("_").rotate.join(".")
+
+      service = service_sym.to_s.split('_').rotate.join('.')
       call(service, **args)
     end
 
     def call(service_name, **args)
       puts "\n#{service_name}: #{args}" if @verbose
       request_xml = MediaFlux.to_xml({
-        request: {
-          service: {
-            _name: service_name,
-            _session: @session,
-            args: args
-          }
-        }
-      })
+                                       request: {
+                                         service: {
+                                           _name: service_name,
+                                           _session: @session,
+                                           args: args
+                                         }
+                                       }
+                                     })
       puts request_xml if @verbose
       response = post(request_xml)
-      response_type = response.elements["/response/reply/@type"].value
-      if response_type == "error" then
-        raise MediaFlux::MFApiError.new(response)
-      elsif response_type == "result"
-        # Note: "reply/result" are not in the docs
-        result = response.elements["/response/reply/result"]
+      response_type = response.elements['/response/reply/@type'].value
+      if response_type == 'error'
+        raise MediaFlux::MFApiError, response
+      elsif response_type == 'result'
+        # NOTE: "reply/result" are not in the docs
+        result = response.elements['/response/reply/result']
         puts MediaFlux.pretty result if @verbose
-        return result
+        result
       else
-        raise MediaFlux::MFError.new("Unexpected response type '#{response_type}'")
+        raise MediaFlux::MFError, "Unexpected response type '#{response_type}'"
       end
     end
 
-    def session()
+    def session
       result_doc = logon_system(
         domain: @mf_domain,
         user: @mf_username,
         password: @mf_password
       )
-      @session = result_doc.elements["session"].text
+      @session = result_doc.elements['session'].text
 
       yield
 
@@ -88,9 +89,9 @@ module MediaFlux
     def post(body)
       https = Net::HTTP.new(@mf_host, @mf_port)
       https.use_ssl = true
-      request = Net::HTTP::Post.new("__mflux_svc__")
+      request = Net::HTTP::Post.new('__mflux_svc__')
       request.body = body
-      request["Content-Type"] = "text/xml; charset=utf-8"
+      request['Content-Type'] = 'text/xml; charset=utf-8'
       response = https.request(request)
       REXML::Document.new(response.body)
     end
@@ -105,40 +106,39 @@ module MediaFlux
   # - Text can be the value, or can use the "_" key, if there are other attributes.
   def to_xml(hash_or_scalar)
     if hash_or_scalar.class != Hash
-      if hash_or_scalar.class != REXML::Document
-        return hash_or_scalar.to_s.encode(xml: :text)
-      end
+      return hash_or_scalar.to_s.encode(xml: :text) if hash_or_scalar.class != REXML::Document
+
       return hash_or_scalar
-    end 
-    hash_or_scalar.map {|key, value|
+    end
+    hash_or_scalar.map do |key, value|
       if value.is_a? Array
-        value.map {|a_value| to_xml({key => a_value})}.join("")
+        value.map { |a_value| to_xml({ key => a_value }) }.join('')
       else
-        name = key.class == String ? key.sub(/^_/, "-") : key.to_s.gsub("_", "-")
-        if name == "-" then
+        name = key.instance_of?(String) ? key.sub(/^_/, '-') : key.to_s.gsub('_', '-')
+        if name == '-'
           to_xml(value)
-        elsif name[0] == "-" then
+        elsif name[0] == '-'
           attr_name = name[1..-1]
-          %Q{ #{attr_name}=#{value.to_s.encode(xml: :attr)}}
+          %( #{attr_name}=#{value.to_s.encode(xml: :attr)})
         else
-          if value.class == Hash then
-            non_nil = value.select {|k,v| v != nil}
-            attr_other = non_nil.partition {|k,v| (k.to_s[0] == '_' and k != :_)}
+          if value.instance_of?(Hash)
+            non_nil = value.select { |_k, v| !v.nil? }
+            attr_other = non_nil.partition { |k, _v| (k.to_s[0] == '_' and k != :_) }
             attr_value = Hash[attr_other[0]]
             other_value = Hash[attr_other[1]]
           else
-            attr_value = ""
+            attr_value = ''
             other_value = value
           end
           "<#{name}#{to_xml(attr_value)}>#{to_xml(other_value)}</#{name}>"
         end
       end
-    }.join("")
+    end.join('')
   end
 
   def pretty(doc)
     formatter = REXML::Formatters::Pretty.new
     formatter.compact = true
-    formatter.write(doc, "")
+    formatter.write(doc, '')
   end
 end
