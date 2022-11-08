9538c03eb9213d52b89ed7d21b785cad479e6d09
mccalluc <mccalluc@users.noreply.github.com>
2022-11-08 11:38:32 -0500

    auto-correct

--- nokogiri/demo.rb
+++ nokogiri/demo.rb
@@ -10,48 +10,47 @@ config = YAML.load_file(__dir__ + '/config.yaml').map { |k, v| [k.to_sym, v] }.t
 
 mf = MediaFlux::MFClient.new(verbose: true, **config)
 
-
 begin
-  mf.call("foo.bar", "")
+  mf.call('foo.bar', '')
 rescue MediaFlux::MFError => e
   puts e.error
   puts e.message
 end
 
-mf.session() do
-  doc = mf.call("asset.namespace.list", "")
-  doc.elements["//namespace"].each do |el|
+mf.session do
+  doc = mf.call('asset.namespace.list', '')
+  doc.elements['//namespace'].each do |el|
     puts el.text
   end
 
   # Based on https://docs.google.com/presentation/d/1GAt7tr2Vb-KJtILbcEcMlr71qxVo0j5-/edit#slide=id.p23
 
-  fragment = Nokogiri::XML::DocumentFragment.parse("")
+  fragment = Nokogiri::XML::DocumentFragment.parse('')
   Nokogiri::XML::Builder.with(fragment) do |xml|
     # xml.name "mouse" # If name is set, and cleanup fails, will not be able to re-run: names are unique.
   end
   args_xml = fragment.to_xml
-  doc = mf.call("asset.create", args_xml)
-  id = doc.elements["//id"].first
+  doc = mf.call('asset.create', args_xml)
+  id = doc.elements['//id'].first
 
-  fragment = Nokogiri::XML::DocumentFragment.parse("")
+  fragment = Nokogiri::XML::DocumentFragment.parse('')
   Nokogiri::XML::Builder.with(fragment) do |xml|
     xml.id id
   end
   args_xml = fragment.to_xml
-  mf.call("asset.get", args_xml)
+  mf.call('asset.get', args_xml)
 
-  fragment = Nokogiri::XML::DocumentFragment.parse("")
+  fragment = Nokogiri::XML::DocumentFragment.parse('')
   Nokogiri::XML::Builder.with(fragment) do |xml|
     xml.id id
-    xml.meta {
-      xml.send("mf-note") {
-        xml.note "Hello"
-      }
-    }
+    xml.meta do
+      xml.send('mf-note') do
+        xml.note 'Hello'
+      end
+    end
   end
   args_xml = fragment.to_xml
-  mf.call("asset.set", args_xml)
+  mf.call('asset.set', args_xml)
 
   # Element and attribute names with dashes are pretty common in the MediaFlux API.
   # If we do go with Nokogiri, we may want a specialized builder to map these names:
@@ -69,18 +68,18 @@ mf.session() do
   #     super(dashed_method, *dashed_args)
   #   end
   # end
-  
-  fragment = Nokogiri::XML::DocumentFragment.parse("")
+
+  fragment = Nokogiri::XML::DocumentFragment.parse('')
   Nokogiri::XML::Builder.with(fragment) do |xml|
     xml.id(id, version: 1)
   end
   args_xml = fragment.to_xml
-  mf.call("asset.get", args_xml)
+  mf.call('asset.get', args_xml)
 
-  fragment = Nokogiri::XML::DocumentFragment.parse("")
+  fragment = Nokogiri::XML::DocumentFragment.parse('')
   Nokogiri::XML::Builder.with(fragment) do |xml|
     xml.id id
   end
   args_xml = fragment.to_xml
-  mf.call("asset.destroy", args_xml)
+  mf.call('asset.destroy', args_xml)
 end
--- nokogiri/mediaflux.rb
+++ nokogiri/mediaflux.rb
@@ -3,9 +3,9 @@ require 'rexml/document'
 require 'nokogiri'
 
 module MediaFlux
-
   class MFError < StandardError
     attr_reader :error, :message, :stack
+
     def initialize(response_doc)
       reply = response_doc.elements['/response/reply']
       @error = reply.elements['error'].text
@@ -16,8 +16,8 @@ module MediaFlux
 
   class MFClient
     def initialize(
-        verbose: false,
-        mf_host:, mf_port:, mf_domain:, mf_username:, mf_password:)
+      mf_host:, mf_port:, mf_domain:, mf_username:, mf_password:, verbose: false
+    )
       @verbose = verbose
       @mf_host = mf_host
       @mf_port = mf_port
@@ -28,45 +28,45 @@ module MediaFlux
 
     def call(service_name, args_xml)
       puts "\n#{service_name}: #{args_xml}" if @verbose
-      builder = Nokogiri::XML::Builder.new() do |xml|
-        xml.request {
-          xml.service(name: service_name, session: @session) {
-            xml.args {
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.request do
+          xml.service(name: service_name, session: @session) do
+            xml.args do
               xml << args_xml
-            }
-          }
-        }
+            end
+          end
+        end
       end
       request_xml = builder.to_xml
       puts request_xml if @verbose
       response = post(request_xml)
-      response_type = response.elements["/response/reply/@type"].value
-      if response_type == "error" then
-        raise MediaFlux::MFError.new(response)
-      elsif response_type == "result"
-        # Note: "reply/result" are not in the docs
-        result = response.elements["/response/reply/result"]
+      response_type = response.elements['/response/reply/@type'].value
+      if response_type == 'error'
+        raise MediaFlux::MFError, response
+      elsif response_type == 'result'
+        # NOTE: "reply/result" are not in the docs
+        result = response.elements['/response/reply/result']
         puts MediaFlux.pretty result if @verbose
-        return result
+        result
       else
         raise MediaFlux::MFError("Unexpected response type '#{response_type}'")
       end
     end
 
-    def session()
-      fragment = Nokogiri::XML::DocumentFragment.parse("")
+    def session
+      fragment = Nokogiri::XML::DocumentFragment.parse('')
       Nokogiri::XML::Builder.with(fragment) do |xml|
         xml.domain @mf_domain
         xml.user @mf_username
         xml.password @mf_password
       end
       args_xml = fragment.to_xml
-      result_doc = call("system.logon", args_xml)
-      @session = result_doc.elements["session"].text
+      result_doc = call('system.logon', args_xml)
+      @session = result_doc.elements['session'].text
 
       yield
 
-      call("system.logoff", "")
+      call('system.logoff', '')
     end
 
     private
@@ -74,9 +74,9 @@ module MediaFlux
     def post(body)
       https = Net::HTTP.new(@mf_host, @mf_port)
       https.use_ssl = true
-      request = Net::HTTP::Post.new("__mflux_svc__")
+      request = Net::HTTP::Post.new('__mflux_svc__')
       request.body = body
-      request["Content-Type"] = "text/xml; charset=utf-8"
+      request['Content-Type'] = 'text/xml; charset=utf-8'
       response = https.request(request)
       REXML::Document.new(response.body)
     end
@@ -87,6 +87,6 @@ module MediaFlux
   def pretty(doc)
     formatter = REXML::Formatters::Pretty.new
     formatter.compact = true
-    formatter.write(doc, "")
+    formatter.write(doc, '')
   end
 end
