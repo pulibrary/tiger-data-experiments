de8e788b390d852115e977936044d092540d1900
carolyncole <1599081+carolyncole@users.noreply.github.com>
2022-11-09 09:14:20 -0500

    Adding a ruby script to list all available services (#25)
    * Adding a ruby script to list all available services
    Also lists available functions
    Includes a simple connection script
    
    * Adding a describe service that can be utilized to describe a service

---
+++ simple-nokogiri/connect.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+def build_request(https, session, name)
+  args = { name: name }
+  args[:session] = session unless session.nil?
+  builder = Nokogiri::XML::Builder.new do |xml|
+    xml.request do
+      xml.service(**args) do
+        yield xml if block_given?
+      end
+    end
+  end
+
+  request = Net::HTTP::Post.new('__mflux_svc__')
+  request.body = builder.to_xml
+  request['Content-Type'] = 'text/xml; charset=utf-8'
+
+  https.request(request)
+end
+
+def login(https, config)
+  response = build_request(https, nil, 'system.logon') do |xml|
+    xml.args do
+      xml.domain config[:mf_domain]
+      xml.user config[:mf_username]
+      xml.password config[:mf_password]
+    end
+  end
+
+  doc = Nokogiri::XML.parse(response.body)
+  doc.xpath('response/reply/result/session').text
+end
+
+def logoff(https)
+  build_request(https, nil, 'system.logoff')
+end
